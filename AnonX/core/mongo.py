67e661e2e62ff898908f67523f0461fd2be738ca
Saykkuno (Ashutosh) <130449575+aashuxxD@users.noreply.github.com>
2024-03-29 00:10:03 +0530

    Update mongo.py

--- AnonX/core/mongo.py
+++ AnonX/core/mongo.py
@@ -9,4 +9,4 @@ from ..logging import LOGGER
 _mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
 _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
 mongodb = _mongo_async_.Inflex
-pymongodb = _mongo_sync_.Inflex
+pymongodb = _mongo_sync_.Anon
